805d6689c4a9b8badcfca5e70fb1980f8e63dd4d
Ugo Arangino <ugo@arangino.de>
2017-01-22 21:44:10 +0100

    Add Example main.swift

--- Example/Sources/main.swift
+++ Example/Sources/main.swift
@@ -1,1 +1,24 @@
-print("Hello, world!")
+import Foundation
+import SigSwift
+
+public enum State {
+    case started
+    case stopped
+}
+
+var state: State = .started
+
+SignalController.add(.INT) { (signalNumber) in
+    guard let signal = Signal(rawValue: signalNumber) else {
+        return
+    }
+    
+    state = .stopped
+    print("Intercepted signal: \(signal)")
+    print("Set state to .stopped")
+}
+
+repeat {
+    print("hello \(Date())")
+    sleep(2)
+} while (state == .started)
